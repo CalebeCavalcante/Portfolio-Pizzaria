696252c8133e6dab931da610468c4832d15dd03d
CalebeCavalcante <calebesantos.cavalcante@gmail.com>
2018-09-26 21:15:28 -0300

    Update Loja

--- site/resources/views/loja.blade.php
+++ site/resources/views/loja.blade.php
@@ -1,57 +1,54 @@
 @extends('layouts.site')
 
 @section('body')
-		<img id="dely_body_img" src="{{asset('img/pizza_caixa.jpg')}}"/>
-		<div id="dely_container" class="container">	
-			<div>
-				<div id="dely_delivery">
-					<span>Lojas</span>
-				</div>
-				<div id="dely_bg_image_logo" class="float-right">
-					<img class="rounded-circle" src="{{asset('img/logo_bg_white.png')}}" />
-				</div>				
+
+	<div id="delivery-body" class="fullscreen-content d-flex justify-content-center align-items-center">
+		<div class="fullscreen-content absolute">
+			<div class="h-100">
+				<img class="fullscreen-img" src="{{ asset('img/pizza_caixa.jpg')  }}" />
 			</div>
-			<div>
-				<div id="dely_bg_image_casa">
-					<div class="element svg" style="width: 197px; height: 107px; transform: translate(-40px, -100px); opacity: 1;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 588.32 650.32" style="position: absolute;">
- <defs>
-  <filter id="CANVA2_d">
-   <feGaussianBlur in="SourceGraphic" stdDeviation="0.9"></feGaussianBlur>
-  </filter>
-  <linearGradient id="CANVA2_c" x1=".94948" x2="-.000006" y1=".49367" y2=".49998">
-   <stop offset="0" stop-color="#888"></stop>
-   <stop offset="1" stop-color="#333"></stop>
-  </linearGradient>
- </defs>
- <g><rect fill="#666666" height="213.36" transform="matrix(1.5281 0 0 1.5281 53.876 -43.819)" width="311.08" x="2.1474" y="184.7" class="color color-1" data-color="#cccccf"></rect></g>
- <g><rect fill="#000000" height="198.7" stroke-width="48" transform="matrix(1.5812 0 0 1.5281 53.839 -43.819)" width="298.05" x="2.0119" y="199.41" class="color color-2" data-color="#bfbfbf"></rect></g>
- <g><rect fill="#666666" filter="url(#CANVA2_d)" height="129.48" stroke-width="0" transform="matrix(1.5281 0 0 1.5281 53.876 -43.819)" width="65.962" x="59.036" y="260.51" class="color color-3" data-color="#7f7f7f"></rect></g>
- <rect fill="url(#CANVA2_c)" height="62.391" opacity=".75" transform="matrix(1.5281 0 0 1.5281 53.876 -43.819)" width="129.5" x="147.24" y="258.77"></rect>
- <g><path d="m60.15 199.96v84.334c0 14.999 12.078 27.077 27.077 27.077h10.267c12.362 0 22.759-8.2018 26.026-19.484 3.2708 11.276 13.573 19.484 25.931 19.484h10.267c12.358 0 22.708-8.2077 25.978-19.484 3.267 11.282 13.617 19.484 25.978 19.484h10.267c11.661 0 21.531-7.3231 25.358-17.621 3.8223 10.306 13.69 17.621 25.358 17.621h10.267c11.06 0 20.525-6.5822 24.737-16.046 4.212 9.4633 13.677 16.046 24.737 16.046h10.267c10.087 0 18.836-5.4701 23.495-13.61 4.6604 8.137 13.41 13.61 23.495 13.61h10.267c9.25 0 17.373-4.6124 22.254-11.652 4.8821 7.0266 12.966 11.652 22.206 11.652h10.267c8.1531 0 15.435-3.5974 20.391-9.2643 4.956 5.667 12.238 9.2643 20.391 9.2643h10.267c14.999 0 27.077-12.078 27.077-27.077v-84.334h-472.62z" fill="#666666" class="color color-3" data-color="#7f7f7f"></path></g>
- <g fill="inherit" transform="matrix(1.5281 0 0 1.1956 53.876 75.824)" class="color color-4" data-color="#ff7f00">
-  <path d="m306.44 10.734v151.47c0 9.815 7.9038 17.719 17.719 17.719h7.6875c9.815 0 17.719-7.9038 17.719-17.719v-9.8125l-43.125-141.66z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m52.819 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6562c9.8149 0 17.719-7.9038 17.719-17.719v-154.44h-43.094z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m10.319 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6875c9.8149 0 17.719-7.9038 17.719-17.719v-154.44h-43.125z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m95.288 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6875c9.8149 0 17.719-7.9038 17.719-17.719v-154.44h-43.125z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m137.16 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6562c9.815 0 17.719-7.9038 17.719-17.719v-154.44h-43.094z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m180.26 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6875c9.815 0 17.719-7.9038 17.719-17.719v-154.44h-43.125z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m221.85 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6875c9.8149 0 17.719-7.9038 17.719-17.719v-154.44h-43.125z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m264.6 7.7647v154.44c0 9.815 7.9038 17.719 17.719 17.719h7.6875c9.815 0 17.688-7.9038 17.688-17.719v-154.44h-43.094z" fill="inherit" class="color color-4" data-color="inherit"></path>
-  <path d="m9.1943 7.7647l-41.969 136.12v18.312c0 9.815 7.9038 17.719 17.719 17.719h7.6562c9.8149 0 17.719-7.9038 17.719-17.719v-154.44h-1.125z" fill="inherit" class="color color-4" data-color="inherit"></path>
- </g>
- <g><rect fill="#666666" height="19.544" transform="matrix(1.5281 0 0 1.5281 53.876 -43.819)" width="300.54" x="7.6334" y="63.804" class="color color-3" data-color="#7f7f7f"></rect></g>
- <g><rect fill="#000000" height="0" stroke-width="48" width="514.97" x="-240.77" y="453.47" class="color color-2" data-color="#bfbfbf"></rect></g>
-</svg></div>
+		</div> 
+		<div class="bg-opaco">
+			<div class="row w-100 h-100">
+				<div class="col-12">
+						<div class="row h-100">
+							<div class="col-6 d-flex align-items-star justify-content-start">
+								<div id="peca-agora" class="my-0 mx-auto h-50 d-flex justify-content-center align-items-center">
+									<span class="text-white mx-2 p-2">PEÇA AGORA</span>
+								</div>
+								<div class="h-75 my-0 d-flex justify-content-center aling-items-center">
+									<img class="h-100 w-100" src="{{ asset('img/icon_forma_red.png') }}">
+								</div>
+							</div>
+							<div class="col-6 d-flex align-items-star justify-content-end">
+								<div id="logo-white" class="h-75 w-75 my-3 d-flex justify-content-center">
+									<img src="{{ asset('img/logo_bg_white.png')  }}" />
+								</div>
+							</div>								
+						</div>	
 				</div>
-			</div>
-			<div>
-				<form>
-					<div class="form-group">
-						<input class="text-center form-control"  type="text" name="cep" placeholder="DIGITE SEU CEP"/>
-						<input class="text-center form-control btn" type="submit" name="buscar" value="LOJAS NA REGIÃO"/>
+				
+				<div class="col-12">
+					<div class="row h-100 d-flex justify-content-center align-items-center">
+						
+						<div class="col-12 h-50 d-flex align-items-center justify-content-center">
+							<div class="h-100 w-50 d-flex align-items-center justify-content-center">
+								<img class="logo-delivery" src="{{ asset('img/icon_casa.png')  }}" />
+							</div>
+						</div>
+
+						<div class="col-12 h-50">
+							<form class="m-auto">
+								<div class="form-group">
+									<input class="text-center form-control" type="text" name="cep" placeholder="DIGITE SEU CEP">
+									<input class="text-center form-control btn" type="submit" name="buscar" value="LOJAS NA REGIÃO">
+								</div>
+							</form>
+						</div>
 					</div>
-				</form>
+				</div>
+				
 			</div>
 		</div>
-		
+	</div>
 @endsection
